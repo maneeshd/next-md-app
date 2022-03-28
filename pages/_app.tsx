0f060cd05fba44bc8d8a25335b3718af359594ec
maneeshd <maneeshd77@gmail.com>
2022-03-28 01:32:45 -0700

    feat(ui-library): Add chakra-ui & react-icons packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,26 +4,36 @@ import ErrorBoundary from '../components/ErrorBoundary'
 import { Provider } from 'react-redux'
 import store from '../store'
 import Link from 'next/link'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
   const { pathname } = router
 
   return (
-    <ErrorBoundary>
-      <Provider store={store}>
-        {pathname !== '/' && (
-          <div className="w-screen absolute top-0 left-0">
-            <Link href="/">
-              <a className="font-mono font-semibold">&lt; Home</a>
-            </Link>
-          </div>
-        )}
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+      </Head>
 
-        <Component {...pageProps} />
-      </Provider>
-    </ErrorBoundary>
+      <ErrorBoundary>
+        <ChakraProvider>
+          <Provider store={store}>
+            {pathname !== '/' && (
+              <div className="w-screen absolute top-0 left-0">
+                <Link href="/">
+                  <a className="font-mono font-semibold">&lt; Home</a>
+                </Link>
+              </div>
+            )}
+            <Component {...pageProps} />
+          </Provider>
+        </ChakraProvider>
+      </ErrorBoundary>
+    </>
   )
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
-
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
